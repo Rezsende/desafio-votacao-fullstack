99f10963fc2bc4fdc9fdec9fb1c6697eca9b15aa
João Paulo <jpresendejava@gmail.com>
2025-04-24 09:22:01 -0400

    Subindo a aplicação

---
+++ desafioVotacaoBackend/src/main/java/com/desafioVotacaoBackend/desafioVotacaoBackend/DesafioVotacaoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.desafioVotacaoBackend.desafioVotacaoBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioVotacaoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioVotacaoBackendApplication.class, args);
+	}
+
+}
---
+++ desafioVotacaoBackend/src/test/java/com/desafioVotacaoBackend/desafioVotacaoBackend/DesafioVotacaoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioVotacaoBackend.desafioVotacaoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioVotacaoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
