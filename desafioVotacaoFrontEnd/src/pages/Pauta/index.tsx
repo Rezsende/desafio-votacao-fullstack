a24dae5c0c199664af6041dd2dba31f11d8a2b41
João Paulo <jpresendejava@gmail.com>
2025-04-29 15:40:58 -0400

    finalizando o front

--- desafioVotacaoFrontEnd/src/pages/Pauta/index.tsx
+++ desafioVotacaoFrontEnd/src/pages/Pauta/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Datapauta } from "./hooks/data_fecht";
 import { usePautaSessaoMutation } from "./hooks/data_postIniciarsessao";
 import { useForm } from "react-hook-form";
@@ -7,6 +7,8 @@ import { z } from "zod";
 import { schemaVoto } from "./schema/schemaVoto";
 import { useVotoMutation } from "./hooks/data_postVotacao";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 type CriarVotoFormData = z.infer<typeof schemaVoto>;
 export const Index = () => {
   const { data, isLoading, isError } = Datapauta();
@@ -16,6 +18,8 @@ export const Index = () => {
   const [isModalVotacaoOpen, setIsModalVotacaoOpen] = useState(false);
   const [tempo, setTempo] = useState(1);
   const [idSelecionado, setIdSelecionado] = useState<number | null>(null);
+  const [tempoRestante, setTempoRestante] = useState<{ [key: number]: number }>({});
+  const [votosContabilizados, setVotosContabilizados] = useState<{ [key: number]: { SIM: number; NAO: number } }>({});
 
   const {
     register,
@@ -59,6 +63,39 @@ export const Index = () => {
     } catch (error) {}
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const novoTempo: typeof tempoRestante = {};
+
+      data?.forEach((pauta: any) => {
+        if (pauta.sessaoAtiva) {
+          const fechamento = new Date(pauta.dataFechamento).getTime();
+          const agora = new Date().getTime();
+          const restante = Math.max(Math.floor((fechamento - agora) / 1000), 0);
+          novoTempo[pauta.id] = restante;
+
+          if (restante === 0 && !votosContabilizados[pauta.id]) {
+            axios
+              .get(`http://localhost:8080/api/votos/contabilizar/${pauta.id}`)
+              .then((res) => {
+                setVotosContabilizados((prev) => ({
+                  ...prev,
+                  [pauta.id]: res.data,
+                }));
+              })
+              .catch((err) => {
+                console.error("Erro ao contabilizar votos:", err);
+              });
+          }
+        }
+      });
+
+      setTempoRestante(novoTempo);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [data, votosContabilizados]);
+
   if (isLoading) {
     return (
       <div className="flex justify-center items-center h-screen">
@@ -89,13 +126,15 @@ export const Index = () => {
             <div
               key={pauta.id}
               className={`bg-white overflow-hidden shadow rounded-lg transition-all duration-300 hover:shadow-lg ${
-                pauta.sessaoAtiva ? "border-l-8 border-green-500" : "border-l-8 border-red-500"
+                !pauta.sessaoAtiva ? "border-l-8 border-red-500" : tempoRestante[pauta.id] === 0 ? "border-l-8 border-yellow-600" : "border-l-8 border-green-500"
               }`}
             >
               <div className="px-4 py-5 sm:p-6">
                 <div className="flex items-center">
                   <div className={`flex-shrink-0 h-12 w-12 rounded-full flex items-center justify-center ${pauta.sessaoAtiva ? "bg-green-100" : "bg-red-100"}`}>
-                    <span className={`${pauta.sessaoAtiva ? "text-green-600" : "text-red-600"} font-bold`}>{pauta.sessaoAtiva ? "A" : "I"}</span>
+                    <span className={`font-bold ${!pauta.sessaoAtiva ? "text-red-600" : tempoRestante[pauta.id] === 0 ? "text-yellow-600" : "text-green-600"}`}>
+                      {pauta.sessaoAtiva ? "A" : "I"}
+                    </span>
                   </div>
                   <div className="ml-4">
                     <h3 className="text-lg leading-6 font-medium text-gray-900">{pauta.titulo}</h3>
@@ -106,7 +145,10 @@ export const Index = () => {
                 <div className="mt-4">
                   <div className="flex justify-between text-sm text-gray-600">
                     <span>Status:</span>
-                    <span className={`font-semibold ${pauta.sessaoAtiva ? "text-green-600" : "text-red-600"}`}>{pauta.sessaoAtiva ? "Ativa" : "Inativa"}</span>
+
+                    <span className={`font-semibold ${pauta.sessaoAtiva ? (tempoRestante[pauta.id] === 0 ? "text-yellow-600 " : "text-green-600") : "text-red-600"}`}>
+                      {pauta.sessaoAtiva ? (tempoRestante[pauta.id] === 0 ? "Encerrada" : "Ativa") : "Inativa"}
+                    </span>
                   </div>
 
                   {pauta.sessaoAtiva ? (
@@ -117,12 +159,16 @@ export const Index = () => {
                       <div>
                         <span className="font-medium">Fechamento:</span> {new Date(pauta.dataFechamento).toLocaleString()}
                       </div>
-                      <button
-                        onClick={() => abrirModalVotacao(pauta.id)}
-                        className="mt-4 w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all"
-                      >
-                        Vota
-                      </button>
+                      {tempoRestante[pauta.id] > 0 ? (
+                        <button
+                          onClick={() => abrirModalVotacao(pauta.id)}
+                          className="mt-4 w-full inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all"
+                        >
+                          Vota
+                        </button>
+                      ) : (
+                        ""
+                      )}
                     </div>
                   ) : (
                     <button
@@ -131,6 +177,17 @@ export const Index = () => {
                     >
                       Iniciar Sessão
                     </button>
+                  )}
+
+                  {votosContabilizados[pauta.id] && (
+                    <div className="mt-2 text-sm text-gray-700">
+                      <p>
+                        <strong>Votos SIM:</strong> {votosContabilizados[pauta.id].SIM}
+                      </p>
+                      <p>
+                        <strong>Votos NÃO:</strong> {votosContabilizados[pauta.id].NAO}
+                      </p>
+                    </div>
                   )}
                 </div>
               </div>
@@ -154,6 +211,7 @@ export const Index = () => {
           </div>
         </div>
       )}
+
       {isModalVotacaoOpen && (
         <form onSubmit={handleSubmit(iniciarVotacoa)}>
           <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
@@ -161,8 +219,19 @@ export const Index = () => {
               <h2 className="text-lg font-semibold mb-4">Votação</h2>
               <label className="text-sm font-semibold">CPF</label>
               <input type="text" {...register("cpf")} className="w-full border border-gray-300 rounded-md px-3 py-2 mb-4" />
+
               <label className="text-sm font-semibold">Opções</label>
-              <input type="text" {...register("opcao")} className="w-full border border-gray-300 rounded-md px-3 py-2 mb-4" />
+              <div className="flex space-x-4 mb-4">
+                <label className="inline-flex items-center">
+                  <input type="radio" {...register("opcao")} value="SIM" className="form-radio h-4 w-4 text-blue-600" />
+                  <span className="ml-2">SIM</span>
+                </label>
+                <label className="inline-flex items-center">
+                  <input type="radio" {...register("opcao")} value="NAO" className="form-radio h-4 w-4 text-blue-600" />
+                  <span className="ml-2">NÃO</span>
+                </label>
+              </div>
+
               <div className="flex justify-end space-x-2">
                 <button onClick={() => setIsModalVotacaoOpen(false)} className="px-4 py-2 bg-gray-300 rounded hover:bg-gray-400">
                   Cancelar
